ab0c0f9e95fcb43286c56157b270979698687689
Ivan-Caringal <ivancaringal20@gmail.com>
2025-06-26 21:46:41 +0800

    feat: refactored code features  (use aplhine js and add a scope for filtering)

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 
+Route::get('/', function () {
+    return redirect()->route('jobs.index');
+});
 //     yung jobs== sa name ng route to http://127.0.0.1:8000/jobs  
 Route::resource('jobs', JobController::class)
     ->only(['index', 'show']);
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
-        require('@tailwindcss/forms'),
+        
         tailwindcss(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
